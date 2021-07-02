30f1901c219c01dad2016fe8750001774d0bbf09
FloLimebit <florian.richter@limebit.de>
2021-07-02 18:38:38 +0200

    plotting fun added

--- notebooks/helper_functions/helper_fun.py
+++ notebooks/helper_functions/helper_fun.py
@@ -1,5 +1,10 @@
 import networkx as nx
 from neo4j import GraphDatabase
+import matplotlib.pyplot as plt
+from matplotlib.colors import rgb2hex
+from itertools import count
+from matplotlib.patches import Circle
+import numpy as np
 
 def to_nx_graph(neo4j_driver, query):
 
@@ -26,3 +31,62 @@ def to_nx_graph(neo4j_driver, query):
                 title=rel.type, 
                 **rel._properties)
     return G
+
+def draw_graph(graph_object, pos = "kamada_kawai_layout", node_labels=None, edge_labels=None, node_color_attribute = "type", color_map= plt.cm.plasma, plot_size=[10,10]):
+
+    if pos == "kamada_kawai_layout":
+        pos = nx.nx.kamada_kawai_layout(graph_object)
+
+    # setup node_colors
+    groups = set(nx.get_node_attributes(graph_object,node_color_attribute).values())
+    group_ids = np.array(range(len(groups)))
+    if len(group_ids) > 1:
+        group_ids_norm = (group_ids - np.min(group_ids))/np.ptp(group_ids)
+    else:
+        group_ids_norm = group_ids
+    mapping = dict(zip(groups, group_ids_norm))
+    node_colors = [mapping[graph_object.nodes()[n][node_color_attribute]] for n in graph_object.nodes()]
+
+    # define plot options
+    node_options = {
+        "pos":  pos,
+        "alpha": 1,
+        "node_size": 150,
+        "alpha": 0.5,
+        "node_color":  node_colors, 
+        "cmap": color_map
+    }
+
+    node_label_options = {
+            "pos": pos,
+            "labels": node_labels,
+            "font_size": 11
+        }
+
+    edge_options = {
+                "pos": pos,
+                "width": 1.5,
+                "alpha": 0.2,
+        }
+
+    plt.figure(figsize=plot_size)
+    plt.margins(x=0.1, y = 0.1)
+
+    # draw the network
+    nx.draw_networkx_nodes(graph_object, **node_options)
+
+    if node_labels:
+        nx.draw_networkx_labels(graph_object, **node_label_options)
+
+    nx.draw_networkx_edges(graph_object, **edge_options)
+
+    if edge_labels:
+        nx.draw_networkx_edge_labels(graph_object, pos = pos, edge_labels=edge_labels) # declare the usage ofthe edge labels
+
+    # create custom legend according to color_map
+
+    geom_list = [Circle([], color = rgb2hex(color_map(float(mapping[term])))) for term in groups]
+    plt.legend(geom_list, groups)
+
+    # show the plot
+    plt.show()
